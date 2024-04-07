09edc8a0b5c6a660447092265686a1d6eecf8891
Firenzo <firenzo@MacBook-Pro-van-Firenzo.local>
2024-04-08 00:10:27 +0200

    upload training exercises

---
+++ src/01-hiring-system/hiring-system.ts
@@ -0,0 +1,12 @@
+const candidate1 = {
+  name: "John",
+  age: 22,
+  city: "Amsterdam",
+};
+
+const candidate2 = {
+  name: "Mary",
+  age: 34,
+  city: "Utrecht",
+  certifcates: ["Accessibility", "Contentstack", "Gitlab"],
+};
---
+++ src/02-custom-enum/custom-enum.ts
@@ -0,0 +1,6 @@
+const STATUS_CODES = {
+  success: 200,
+  forbidden: 403,
+  notFound: 404,
+  internalServerError: 500,
+} as const;
